54adb6cab73b453277bdd066aba78e79cc2a8fe1
Ravisara Jayasundara <139541406+RavisaraJayasundara@users.noreply.github.com>
2024-11-29 23:43:51 +0530

    update

---
+++ project4/src/components/GameBase.jsx
@@ -0,0 +1,17 @@
+export default function GameBase(){
+    return(
+        <div>
+            <table border={1} width='100%'>
+                <tr>
+                    <th colspan={3}> ANIMAL MATCHING GAME</th>
+
+                </tr>
+                <tr>
+                    <th>Result</th>
+                    <th>Animal Name</th>
+                    <th>Select the animal</th>
+                </tr>
+            </table>
+        </div>
+    )
+}
