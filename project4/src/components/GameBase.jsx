525df976b3c3967268c1003d9a73f2398e37f4f4
Ravisara Jayasundara <139541406+RavisaraJayasundara@users.noreply.github.com>
2024-11-30 16:30:06 +0530

    update GameBase.jsx

--- project4/src/components/GameBase.jsx
+++ project4/src/components/GameBase.jsx
@@ -1,17 +1,41 @@
+import{animals} from '../data/AnimalsDb';
+import {useState} from 'react';
+
+
+
 export default function GameBase(){
+   const randomAnimal=()=>{
+    const animalId=Math.floor(Math.random()*10)+1;
+    return animals[animalId].name;
+   }
+    
+   const[targetAnimal,setTargetAnimal]=useState(randomAnimal);
+   const[message,setMessage]=useState('');
+
+   const handleClickAnimal=(selectAnimal)=>{
+       if(targetAnimal===selectAnimal){
+            setMessage('won');
+       }else{
+            setMessage('lose');
+       }
+
+    setTimeout(()=>{
+          setTargetAnimal(randomAnimal());
+          setMessage('');
+    },2000)   
+   }
+
     return(
         <div>
-            <table border={1} width='100%'>
-                <tr>
-                    <th colspan={3}> ANIMAL MATCHING GAME</th>
-
-                </tr>
-                <tr>
-                    <th>Result</th>
-                    <th>Animal Name</th>
-                    <th>Select the animal</th>
-                </tr>
-            </table>
+            
+            <h1>find:{targetAnimal}</h1>
+            <h1>{message}</h1>
+            {animals.map((animal)=>(
+                <button onClick={()=>handleClickAnimal(animal.name)}>
+                    <img src={require('../assets/image/'+animal.img)}></img>
+                </button>
+            ))}
+            
         </div>
-    )
+    );
 }
